c6b214e776b0ce196297831e542e749a18b7ef86
Oscar Ossa <oskrlex17@gmail.com>
2023-04-10 05:53:46 -0500

    mekano data

---
+++ internal/mekano/mekano_datasheet.go
@@ -0,0 +1,27 @@
+package mekano
+
+type MekanoData struct {
+	Tipo          string
+	Prefijo       string
+	Numero        string
+	Secuencia     string
+	Fecha         string
+	Cuenta        string
+	Terceros      string
+	CentroCostos  string
+	Nota          string
+	Debito        string
+	Credito       string
+	Base          string
+	Aplica        string
+	TipoAnexo     string
+	PrefijoAnexo  string
+	NumeroAnexo   string
+	Usuario       string
+	Signo         string
+	CuentaCobrar  string
+	CuentaPagar   string
+	NombreTercero string
+	NombreCentro  string
+	Interface     string
+}
